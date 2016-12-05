c7387bfc42fb146dc43301aab84ad16b9a598101
Anders Schmidt Hansen <me@andersschmidt.com>
2016-12-05 16:50:03 +0100

    first vuex bootable version

--- project_templates/vue/templates/main_template.js
+++ project_templates/vue/templates/main_template.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from 'src/router'
 import store from 'src/store'
-import <%= model.app.appName %> from './<%= model.app.appName %>.vue';
+import <%= model.app.appName %> from './<%= model.app.appName %>.vue'
 
 Vue.use(VueResource)
 
+/* eslint-disable no-new */
 new Vue({
   el: '#<%= model.app.appName.toLowerCase() %>',
   http: {},
   store,
   router,
   render: h => h(<%= model.app.appName %>)
-});
+})
